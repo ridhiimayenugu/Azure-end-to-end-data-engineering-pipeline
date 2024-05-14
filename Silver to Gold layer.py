ce4096cd33bb9f2b70a79a64f6f75943005ff23e
ridhiimayenugu <168448182+ridhiimayenugu@users.noreply.github.com>
2024-05-14 10:22:08 -0400

    Add files via upload

---
+++ Silver to Gold layer.py
@@ -0,0 +1,33 @@
+# Databricks notebook source
+table_name = []
+
+for i in dbutils.fs.ls('mnt/silver/SalesLT/'):
+    table_name.append(i.name.split('/')[0])
+
+# COMMAND ----------
+
+table_name
+
+# COMMAND ----------
+
+for name in table_name:
+    # Assuming "mnt" is the root directory
+    path = '/mnt/silver/SalesLT/' + name
+    print(path)
+    df = spark.read.format('delta').load(path)
+    column_names = df.columns
+
+    for old_col_name in column_names:
+        # Convert column name from ColumnName to Column_Name format
+        new_col_name = "".join(["_" + char if char.isupper() and not old_col_name[i - 1].isupper() else char for i,char in enumerate(old_col_name)]).lstrip("_")
+
+        # Change the column name using withColumnRenamed and regex_replace
+        df = df.withColumnRenamed(old_col_name, new_col_name)
+
+    # Adjusting output path
+    output_path = '/mnt/gold/SalesLT/' + name + '/'
+    df.write.format('delta').mode("overwrite").save(output_path)
+
+# COMMAND ----------
+
+display(df)
